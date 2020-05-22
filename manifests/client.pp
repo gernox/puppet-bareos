7717512e28000baaa615ea2e9a38f6c97c19d01e
Michael Haase <darkstar@gernox.de>
2020-05-22 07:55:00 +0200

    add basic config

--- manifests/client.pp
+++ manifests/client.pp
@@ -10,15 +10,16 @@
 # @param jobs
 #
 class gernox_bareos::client (
-  String $client_name,
-  String $director_name,
+  String $director_hostname,
+  String $director_address,
+  String $client_hostname = $::fqdn,
   String $client_password = fqdn_rand_string(20, '', 'bareos-client-password'),
   Hash $jobs              = {},
 ) {
   contain gernox_bareos::install
 
   class { '::bareos::client::client':
-    name_client => $client_name,
+    name_client => $client_hostname,
     # tls_enable  => false,
   }
 
@@ -33,13 +34,14 @@ class gernox_bareos::client (
   }
 
   # allow bareos server to connect
-  ::bareos::client::director { $director_name:
+  ::bareos::client::director { $director_hostname:
+    address  => $director_address,
     password => $client_password,
     # tls_enable => false,
   }
 
   ::bareos::client::messages { 'Standard':
     description => 'Send relevant messages to the Director.',
-    director    => "${director_name} = all, !skipped, !restored",
+    director    => "${director_hostname} = all, !skipped, !restored",
   }
 }
