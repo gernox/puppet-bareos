df200c7cd3b1b176258d064ebe74a912ef48a53f
Michael Haase <darkstar@gernox.de>
2020-06-01 15:11:05 +0200

    Fix bareos webui package

--- manifests/director/webui.pp
+++ manifests/director/webui.pp
@@ -5,7 +5,7 @@ class gernox_bareos::director::webui (
   String $version,
 ) {
   class { 'bareos::webui':
-    package_ensure   => true,
+    package_ensure   => present,
     service_ensure   => false,
     service_enable   => false,
     manage_local_dir => true,
