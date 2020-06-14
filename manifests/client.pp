a63bfc7d11598133ae65351fb56491112982c857
Michael Haase <darkstar@gernox.de>
2020-06-14 16:17:20 +0200

    Enable bareos data encryption

--- manifests/client.pp
+++ manifests/client.pp
@@ -14,12 +14,30 @@
 class gernox_bareos::client (
   String $director_hostname,
   String $director_address,
+  String $pki_key_pair,
+  String $pki_master_key,
   String $client_hostname = $::fqdn,
   String $client_address  = $::fqdn,
   String $client_password = fqdn_rand_string(20, '', 'bareos-client-password'),
   Hash $jobs              = {},
 ) {
   contain gernox_bareos::install
+
+  file { '/etc/bareos/pki.pem':
+    ensure  => present,
+    owner   => 'bareos',
+    group   => 'bareos',
+    mode    => '0640',
+    content => $pki_key_pair,
+  }
+
+  file { '/etc/bareos/master.cert':
+    ensure  => present,
+    owner   => 'bareos',
+    group   => 'bareos',
+    mode    => '0644',
+    content => $pki_master_key,
+  }
 
   class { '::bareos::client::client':
     name_client             => $client_hostname,
@@ -30,6 +48,11 @@ class gernox_bareos::client (
     tls_certificate         => '/etc/bareos/tls/cert.pem',
     tls_key                 => '/etc/bareos/tls/key.pem',
     tls_dh_file             => '/etc/bareos/tls/dh.pem',
+    pki_signatures          => true,
+    pki_encryption          => true,
+    pki_cipher              => 'aes256',
+    pki_key_pair            => '/etc/bareos/pki.pem',
+    pki_master_key          => '/etc/bareos/master.cert',
   }
 
   # allow bareos server to connect
